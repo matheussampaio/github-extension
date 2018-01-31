9ae317cd7723faae4bcb9767aa76dfa3dbc2198b
Matheus Sampaio <matheus@sampaio.us>
2018-01-31 00:02:53 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@
 // @name         Filter News Timeline
 // @namespace    https://github.com/matheussampaio/github-extension
 // @version      1.0.0
+// @license      MIT
 // @description  Filter News Timeline by push, stars, etc.
 // @author       Matheus Sampaio <matheus@sampaio.us>
 // @match        https://github.com/
 // @grant        GM_addStyle
+// @homepageURL  https://github.com/matheussampaio/github-extension
+// @supportURL   https://github.com/matheussampaio/github-extension/issues
+// @downloadURL  https://raw.githubusercontent.com/matheussampaio/github-extension/master/src/index.js
+// @updateURL    https://raw.githubusercontent.com/matheussampaio/github-extension/master/src/index.js
 // ==/UserScript==
 
 (function() {
     'use strict';
     GM_addStyle('.news.column > .push { display: none !important; }');
-    
 })();
